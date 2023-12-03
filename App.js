dcf98e82478909ceaf348556ce4d07b05462c69b
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-03 15:52:10 +0530

    Usage of React.createElement( )

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "First Heading Tag!"),
+    React.createElement("h1", {}, "Second Heading Tag!"),
+  ])
+);
 const heading = React.createElement(
   "h1",
   { id: "heading", className: "head" },
   "Hello World from React!"
 );
+console.log(heading);
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
