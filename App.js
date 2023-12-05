3a08aa4d1c014ff9d49a3922c40b2f25b1b57cd6
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-05 14:55:00 +0530

    jsx object to understand jsx

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // React Element --> Object --> Renders element on the DOM, it becomes an HTML Element.
-
 const heading = React.createElement(
   "h1",
   { id: "heading" },
   "Namaste React 🚀"
 );
+console.log(heading);
+
+// JSX --> JSX is a HTML like Syntax --> It is transpiled before it reaches to JavaScript Engine by Babel --> It converts into React.createElement() --> It converts into object.
+const jsxHeading = <h1 id="heading">Namaste React using JSX 🚀</h1>;
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(jsxHeading);
