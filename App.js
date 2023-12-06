2881464187672727f3c4d5db5582feae0ce0320f
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-06 14:43:52 +0530

    Header Component Created

--- App.js
+++ App.js
@@ -1,34 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-// React Element --> Object --> Renders element on the DOM, it becomes an HTML Element.
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Namaste React 🚀"
-);
-console.log(heading);
-
-// JSX --> JSX is a HTML like Syntax --> It is transpiled before it reaches to JavaScript Engine by Babel --> It converts into React.createElement() --> It converts into object.
-const jsxHeading = <h1 className="head">Namaste React using JSX 🚀</h1>;
-console.log(jsxHeading);
-
-// React Component
-
-// Class Based Components
-// Functional Based Components
-
-const Title = () => {
-  return <h1>Namaste React Title Component🚀</h1>;
+const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <div className="logo-container">
+          <img
+            src="https://images-platform.99static.com/A_Ax0GQuo_NHI0Y7XZHmFtGfBDY=/0x0:1000x1000/500x500/top/smart/99designs-contests-attachments/126/126252/attachment_126252018"
+            alt="Food-App-Logo"
+            className="logo"
+          />
+        </div>
+        <nav className="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Conatct</li>
+            <li>Cart</li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 };
-const FunctionalComponent = () => (
-  <>
-    {/* Component Composition */}
-    <Title />
-    <h1>Namaste React Functional Component Two🚀 </h1>
-  </>
-);
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(jsxHeading);
-// root.render(<HeadingComponentOne />);
-root.render(<FunctionalComponent />);
+root.render(<AppLayout />);
