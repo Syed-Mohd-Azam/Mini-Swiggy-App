27a6b1f54f6d2d7fe482a01afd986dcf3fa4ada1
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-08 22:34:34 +0530

    Header Component separated

--- App.js
+++ App.js
@@ -1,28 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const Header = () => {
-  return (
-    <>
-      <header className="header">
-        <div className="logo-container">
-          <img
-            src="https://images-platform.99static.com/A_Ax0GQuo_NHI0Y7XZHmFtGfBDY=/0x0:1000x1000/500x500/top/smart/99designs-contests-attachments/126/126252/attachment_126252018"
-            alt="Food-App-Logo"
-            className="logo"
-          />
-        </div>
-        <nav className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Conatct</li>
-            <li>Cart</li>
-          </ul>
-        </nav>
-      </header>
-    </>
-  );
-};
+import Header from "./src/Header";
 const RestaurantCard = ({ name, cuisines, stars, minutes }) => {
   return (
     <>
