ae6b8da22630d93b8a90f3b357c4066732189362
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-18 21:22:41 +0530

    createBrowserRouter( ) used to configure routing

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./src/Header";
 import Body from "./src/Body";
-
+import { createBrowserRouter } from "react-router-dom";
 const AppLayout = () => {
   return (
     <>
@@ -11,6 +11,21 @@ const AppLayout = () => {
     </>
   );
 };
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<AppLayout />);
