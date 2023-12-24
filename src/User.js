2814a5feac799f4440966071db01090f61f119f3
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-24 19:06:37 +0530

    functionality added on button to show how we can change the value of state variable in Class Component

--- src/User.js
+++ src/User.js
@@ -1,12 +1,10 @@
 import { useState } from "react";
 const User = ({ name, location, contact }) => {
   const [countOne, setCountOne] = useState(0);
-  const [countTwo, setCountTwo] = useState(1);
   return (
     <>
       <main className="border-black border-2 rounded-md m-1 p-4">
         <h1>Count One:{countOne}</h1>
-        <h1>Count Two:{countTwo}</h1>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h4>Contact: {contact}</h4>
--- src/UserClass.js
+++ src/UserClass.js
@@ -5,16 +5,24 @@ class UserClass extends React.Component {
     console.log(props);
     this.state = {
       countOne: 0,
-      countTwo: 1,
     };
   }
   render() {
     const { name, location, contact } = this.props;
-    const { countOne, countTwo } = this.state;
+    const { countOne } = this.state;
     return (
       <main className="border-2 border-black m-2 p-2 rounded-md">
         <h1>Count One: {countOne}</h1>
-        <h1>Count Two: {countTwo}</h1>
+        <button
+          onClick={() => {
+            this.setState({
+              countOne: this.state.countOne + 1,
+            });
+          }}
+          className="bg-yellow-300 text-black border-black border-0 px-2 py-1 rounded-md"
+        >
+          Count Increase
+        </button>
         <h2>Name:{name}</h2>
         <h3>Location:{location}</h3>
         <h4>Contact:{contact}</h4>
