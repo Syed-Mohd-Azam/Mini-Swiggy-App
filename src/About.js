d589ce17e13ff58a2bda7eb6f2d5001b34410f55
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-24 17:08:28 +0530

    User Functional Component Created

--- src/About.js
+++ src/About.js
@@ -1,9 +1,11 @@
+import User from "./User";
 const About = () => {
   return (
     <>
       <main>
         <h1>About Us Page</h1>
         <h2>This is React Series</h2>
+        <User />
       </main>
     </>
   );
---
+++ src/User.js
@@ -0,0 +1,12 @@
+const User = () => {
+  return (
+    <>
+      <main>
+        <h2>Name: Azam</h2>
+        <h3>Location: Dehradun</h3>
+        <h4>Contact:smazamamu@gmail.com</h4>
+      </main>
+    </>
+  );
+};
+export default User;
