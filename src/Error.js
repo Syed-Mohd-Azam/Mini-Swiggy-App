6ab30f3d0ef834c91b6bd691eceda1d7a909a96d
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-30 18:02:13 +0530

    useRouteError() hook and its usage

--- src/Error.js
+++ src/Error.js
@@ -1,5 +1,6 @@
 import { useRouteError } from "react-router-dom";
 const Error = () => {
+  // useRouteError() is a hook provided by React Router to display the error with message of the reason. It is used to cover the error Boundary.
   const error = useRouteError();
   console.log(error);
   return (
