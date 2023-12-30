b36311aa9ae81c063116c11bdc677f163f5ccff6
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-30 18:15:02 +0530

    AppLayout Component  created to decide the structure

--- src/AppLayout.js
+++ src/AppLayout.js
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+// AppLayout Component is created to decide the structure of the application.
 const AppLayout = () => {
   return (
     <>
