eeba62f05572f077301240d2720df05b7518be5c
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-01-01 04:22:29 +0530

    changes done

--- src/RestaurantCategory.js
+++ src/RestaurantCategory.js
@@ -2,7 +2,7 @@ import ItemCards from "./ItemCards";
 const RestaurantCategory = ({ title, itemCards }) => {
   return (
     <>
-      <section className="max-w-4xl mx-auto mt-6 mb-4 bg-pink-200 rounded-xl shadow-lg p-4">
+      <section className=" mx-auto mt-6 mb-4 bg-pink-200 rounded-xl shadow-lg p-4">
         <article className="flex justify-between  ">
           <p className="text-white font-bold text-lg">
             {title} ({itemCards?.length})
