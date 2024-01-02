d938c48b73b37af621776879b26bc44429768a02
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-01-02 20:38:11 +0530

    Toggle functionality added

--- src/RestaurantCategory.js
+++ src/RestaurantCategory.js
@@ -1,30 +1,37 @@
+import { useState } from "react";
 import ItemCards from "./ItemCards";
 const RestaurantCategory = ({ title, itemCards }) => {
+  const [showItems, setShowItems] = useState(false);
   return (
     <>
-      <section className=" mt-6 mb-4 bg-pink-300 rounded-xl shadow-lg p-4">
+      <section
+        className="mt-6 mb-4 bg-pink-300 rounded-xl shadow-lg p-7 cursor-pointer"
+        onClick={() => setShowItems(!showItems)}
+      >
         <article className="flex justify-between  ">
           <p className="text-white font-bold text-xl">
             {title} ({itemCards?.length})
           </p>
-          <p className="text-white font-bold">▲</p>
+          <p className="text-white font-bold">{showItems ? "▼" : "▲"}</p>
         </article>
-        <article>
-          {itemCards?.length > 0 ? (
-            <>
-              <ItemCards itemCards={itemCards} />
-            </>
-          ) : (
-            <>
-              <section className="p-5 m-5">
-                <span className=" text-3xl">🤔 </span>
-                <span className=" text-white font-bold text-2xl">
-                  Sorry!! No Item Cards
-                </span>
-              </section>
-            </>
-          )}
-        </article>
+        {showItems && (
+          <article>
+            {itemCards?.length > 0 ? (
+              <>
+                <ItemCards itemCards={itemCards} />
+              </>
+            ) : (
+              <>
+                <section className="p-5 m-5">
+                  <span className=" text-3xl">🤔 </span>
+                  <span className=" text-white font-bold text-2xl">
+                    Sorry!! No Item Cards
+                  </span>
+                </section>
+              </>
+            )}
+          </article>
+        )}
       </section>
     </>
   );
