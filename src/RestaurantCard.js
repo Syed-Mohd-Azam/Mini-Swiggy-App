bcd13b8cdd4d48b73bda7982299fee0e072c3947
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-01-05 22:30:07 +0530

    effects added to RestaurantCard Component

--- src/RestaurantCard.js
+++ src/RestaurantCard.js
@@ -17,14 +17,11 @@ const RestaurantCard = ({
     <>
       <section className="w-72 h-80 rounded-t-2xl  ">
         <img
-          className="w-full h-1/2 rounded-2xl mb-2 hover:scale-90 bg-blend-darken shadow-xl  hover:brightness-50"
+          className="w-full h-1/2 rounded-t-2xl  hover:scale-90 bg-blend-darken   hover:brightness-50"
           src={SWIGGY_IMAGE_CLOUDINARY + cloudinaryImageId}
           alt="Restaurant Logo"
         />
-        <section
-          style={{ backgroundColor: "#f0f0f0" }}
-          className="w-full h-1/2 rounded-2xl px-3 py-3 hover:skew-y-6 shadow-xl"
-        >
+        <section className="w-full h-1/2 rounded-b-2xl px-3 py-3  shadow-xl text-white hover:scale-110">
           <h1 className="text-orange-800 italic font-bold mb-1">{name}</h1>
           <h5 className="text-stone-400 font-bold">{`${cuisines
             .slice(0, 2)
@@ -44,7 +41,7 @@ export const withPromotedLabel = (RestaurantCard) => {
     return (
       <>
         <section className="relative">
-          <label className="font-bold absolute top-0 left-0 p-1 bg-gradient-to-r from-purple-500 to-pink-500 text-white z-10 rounded-lg">
+          <label className="font-bold absolute top-0 left-0  rotate-30 bg-gradient-to-r from-purple-500 to-pink-500 text-white z-10 rounded-2xl p-2">
             {restaurant?.info?.aggregatedDiscountInfoV3?.header}{" "}
             {restaurant?.info?.aggregatedDiscountInfoV3?.subHeader}
           </label>
